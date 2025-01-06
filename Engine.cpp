ee6f83a1764a50682b2ef42d0ef4b095038d8a1d
ShadowSL3 <mizasp71@gmail.com>
2025-01-06 16:41:55 +0100

    Updated Engine.cpp Removed SDL_Image.h To internal Folder Added imports For SFML Updated Makefile
    Good morning everyone, today I’ve improved the engine and reworked part of the project. It’s all set and ready to go. SDL_Image.h is now supported in the includes, and I’ve added -lSDL2_image to the Makefile. The engine now also supports SFML imports, preparing for the 1.0 update.

--- Engine.cpp
+++ Engine.cpp
@@ -1,15 +1,28 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <SDL2/SDL_image.h>
 
-
-
+const int _WIDTH = 650;
+const int _HEIGHT = 512;
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Usaneo Engine", 100, 100, 600, 512,SDL_WINDOW_SHOWN);
+    std::string windowTitle = "Usaneo";
+    // SDL_Surface* imageSurface = nullptr;
+    SDL_Window* window = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _WIDTH, _HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
     SDL_Renderer* render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(render, 84, 85, 255, 255);
-    SDL_GetCurrentVideoDriver();
+    SDL_Event evt;
+    bool quit = false;
+    // if(!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_JPG)){
+    //     std::cout << "Error loading image" << std::endl;
+    // }
+    while(!quit) { 
+        while(SDL_PollEvent(&evt) != 0){
+            if (evt.type == SDL_QUIT) { 
+                quit = true;
+            }
+        }
+    }
     SDL_RenderClear(render);
     SDL_RenderPresent(render);
-    SDL_Delay(5000);
+    return 0;
 }
---
+++ core/imports/sfml_load.cpp
@@ -0,0 +1,11 @@
+#include "sfml_load.h"
+
+namespace sfm {
+
+SFML_Load::SFML_Load() { 
+    
+}
+void SFML_Load::window_init(){
+
+}
+}
