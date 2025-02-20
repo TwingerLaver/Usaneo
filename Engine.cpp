1155da9175613e74f46fea6186c31b9ad921ed92
ShadowSL3 <mizasp71@gmail.com>
2025-02-20 11:47:03 +0100

    SDLK_QUIT Removed, Added Surface Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -5,11 +5,7 @@
 #include "SDL2/SDL_ttf.h"
 #include <core/maths/maths.h>
 #include "core/include/InputManager.hpp"
-#define RQUIT SDLK_DELETE
-void use_windows() {
-     
-}
-
+#include "core/Image.h"
 void debug(SDL_Renderer* renderer){
     
     // Add Debug Mode
@@ -25,15 +21,13 @@ void debug(SDL_Renderer* renderer){
 const int _WIDTH = 650;
 const int _HEIGHT = 512;
 int main(int argc, char* argv[]) {
-    use_windows();
     SDL_Init(SDL_INIT_VIDEO);
     std::string windowTitle = "Usaneo Engine";
     SDL_Window* window = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _WIDTH, _HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
     SDL_Renderer* render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_Event evt;
     bool quit = false;
-    
-    
+    SDL_Surface sf; 
     // if(!(IMG_Init(IMG_INIT_PNG))){
     //     std::cout << "Error loading image" << std::endl;
     // }
@@ -42,14 +36,9 @@ int main(int argc, char* argv[]) {
             if (evt.type == SDL_QUIT) { 
                 quit = true;
             }
-            // Note: This statement Function pressed D close Alternative X 
-            if(evt.type == RQUIT){
-                quit = true;
-            }
         }
         debug(render); // Target Render on Rect Debug
     }
-    
     SDL_RenderClear(render);
     SDL_RenderPresent(render);
     return 0;
--- core/Image.h
+++ core/Image.h
@@ -1,24 +1,24 @@
 #include <SDL2/SDL.h>
 
-
-
-class Image : public SDL_Surface, SDL_Rect
-{
-   SDL_Surface sf;
-   SDL_Surface screen;
-    void add_suf(int w, int h){
+class Image{
+    public:
+    SDL_Surface sf;
+    SDL_Surface screen;
+    void add_suf(int w, int h)
+    {
         sf.flags = 0;
-        sf.clip_rect = {0,0,0,0}; 
+        sf.clip_rect = {0, 0, 0, 0};
         sf.format = NULL;
         sf.h = h;
         sf.w = w;
         sf.pitch = 0;
         sf.locked = 0;
-        
     }
-    void drawing_screen(SDL_Surface* surface, int x, int y) {
+    void drawing_screen(SDL_Renderer *render, SDL_Surface *surface, int x, int y, Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+    {
         SDL_LockSurface(surface);
-        SDL_memset(screen.pixels, 255, this->screen.h * this->screen.pitch); 
+        SDL_memset(screen.pixels, 255, this->screen.h * this->screen.pitch);
         SDL_UnlockSurface(surface);
+        SDL_SetRenderDrawColor(render, r, g, b, a);
     }
 };
